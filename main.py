56d332363a2353d8a095f4da8d8bf146cfcc72a9
hallan-n <hallanneves01@gmail.com>
2025-11-18 22:24:33 -0300

    corrigido erro de delay

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ from rich.box import ROUNDED
 
 class FollowLeader:
     console = Console()
-    title = None
     x = None
     y = None
+    leader = None
+
+
+    def set_leader_window(self):
+        self.leader = gw.getActiveWindow()
+        self.show_instructions()
+        print('lider', self.leader._hWnd)
 
     def get_pw_windows(self, title: str):
         return gw.getWindowsWithTitle(title)
@@ -19,35 +25,25 @@ class FollowLeader:
     def record_mouse(self):
         self.x, self.y = pyautogui.position()
         self.show_instructions()
-    
+
     def focus_window(self, window):
-        try:
-            if window.isMinimized:
-                window.restore()
-                time.sleep(0.1)
-        except: pass
-        try:
-            window.activate()
-            time.sleep(0.1)
-        except: pass
-        try:
-            if not window.isActive:
-                window.maximize()
-                time.sleep(0.1)
-                window.activate()
-        except: pass
+        window.restore()
+        time.sleep(0.1)
+        window.activate()
 
 
     def follow_leader(self):
-        windows = self.get_pw_windows('The Classic PW')
+        windows = self.get_pw_windows(self.leader.title)
         for window in windows:
             self.focus_window(window)
-            time.sleep(1)
-            pyautogui.doubleClick(self.x, self.y)
+            
+            time.sleep(0.1)
+            pyautogui.tripleClick(self.x, self.y)
+            time.sleep(0.1)
+            pyautogui.tripleClick(self.x, self.y)
 
-    def set_title(self):
-        self.title = input("Nome da janela do PW: ").strip()
-        self.show_instructions()
+        self.focus_window(self.leader)
+
 
     def build_instructions_table(self) -> Table:
         table = Table(title="Atalhos", box=ROUNDED, show_edge=True, expand=False)
@@ -55,11 +51,11 @@ class FollowLeader:
         table.add_column("Tecla", justify="center", style="bold yellow", no_wrap=True)
         table.add_column("Ação", style="bold white")
         table.add_column("Valor atual", style="bold red")
-
-        table.add_row("F7", "Inserir nome da janela do PW", self.title or "Aguardando")
+        
+        table.add_row("F7", "Selecione a janela do lider da PT", "Selecionado" if self.leader else "Aguardando")
         table.add_row("F8", "Gravar localização do líder da PT", f"{self.x}, {self.y}" if self.x else "Aguardando")
-        table.add_row("F9", "Todos seguirem o líder", "Aguardando")
-        table.add_row("CTRL + C", "Sair do programa", "Aguardando")
+        table.add_row("F9", "Todos seguirem o líder", "-")
+        table.add_row("CTRL + C", "Sair do programa", "-")
         return table    
 
     def show_instructions(self):
@@ -68,7 +64,7 @@ class FollowLeader:
 
     def main(self):
         self.show_instructions()
-        keyboard.add_hotkey('F7', self.set_title)
+        keyboard.add_hotkey('F7', self.set_leader_window)
         keyboard.add_hotkey('F8', self.record_mouse)
         keyboard.add_hotkey('F9', self.follow_leader)
         keyboard.wait()
